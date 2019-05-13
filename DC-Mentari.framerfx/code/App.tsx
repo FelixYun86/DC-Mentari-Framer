3cf901d51315da9ce8356f59bb3308db11711c2d
Felix <felix.yuniar@gmail.com>
2019-05-13 12:26:00 +0700

    Tenor Override
    Added override for onClick function to change tenor value for Loan Calculator

--- DC-Mentari.framerfx/code/App.tsx
+++ DC-Mentari.framerfx/code/App.tsx
@@ -1,9 +1,11 @@
+import * as React from "react";
 import { Override, Data } from "framer";
 
 // Override Docs: https://framer.com/docs/overrides
 
 export const data = Data({
-  value: 2000000
+  value: 2000000,
+  tenorvalue: 12
 });
 
 let result1 = 0;
@@ -16,9 +18,26 @@ export function TextFieldValue(): Override {
     }
   };
 }
+
+export function TapLog(): Override {
+  return {
+    onClick: () => {
+      if (data.tenorvalue === 12) {
+        data.tenorvalue = 24;
+      } else {
+        data.tenorvalue = 12;
+      }
+    }
+  };
+}
+
 export function ApplyValue(): Override {
-  result1 = Math.round(data.value * 0.15);
-  result2 = Math.round(data.value * 0.1);
+  result1 = Math.round(
+    data.value / data.tenorvalue + (data.value / data.tenorvalue) * 0.0175
+  );
+  result2 = Math.round(
+    data.value / data.tenorvalue + (data.value / data.tenorvalue) * 0.01
+  );
   return {
     text: "Rp" + numberWithCommas(result2) + " - Rp" + numberWithCommas(result1)
   };
